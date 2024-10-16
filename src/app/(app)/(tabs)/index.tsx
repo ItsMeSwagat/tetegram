f8781f9dc1a07eae20fcf9a7c542afe1384d5848
ItsMeSwagat <xtha.swagat69@gmail.com>
2024-10-16 22:38:43 +0545

    Setup chaneel

--- src/app/(app)/(tabs)/index.tsx
+++ src/app/(app)/(tabs)/index.tsx
@@ -1,10 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  Channel,
+  ChannelList,
+  MessageInput,
+  MessageList,
+} from "stream-chat-expo";
+import { Channel as ChannelType, StreamChat } from "stream-chat";
+import { router } from "expo-router";
 
 export default function MainTabScreen() {
+  const [channel, setChannel] = useState();
+
+  if (channel) {
+    return (
+      <Channel channel={channel}>
+        <MessageList />
+        <MessageInput />
+      </Channel>
+    );
+  }
   return (
-    <View>
-      <Text>Main TAB</Text>
-    </View>
-  )
+    <ChannelList
+      onSelect={(channel) => router.push(`/channel/${channel.cid}`)}
+    />
+  );
 }
---
+++ src/app/(app)/channel/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function ChannelScreen() {
+  return (
+    <View>
+      <Text>Channel</Text>
+    </View>
+  )
+}
